277a1a061efd5a2add926f7e5c9cde4fa1cc952a
Tiffanie McCoy <tiff.mccoy@gmail.com>
2015-07-13 21:54:10 -0700

    using Cocoapod to load Alamofire added basic http support with Alamofire GPS lookup for iOS 8 is working

--- Mosquito/ViewController.swift
+++ Mosquito/ViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import CoreLocation
+import Foundation
+import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,77 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        println("inside didChangeAuthorizationStatus")
+        if status == .AuthorizedAlways || status == .AuthorizedWhenInUse {
+            locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        println("inside didUpdateLocations")
+        //grab the location and do something with it
+        var latestLocation = locations.last
+        let coords = latestLocation!.coordinate
+        println("coords: " + coords.latitude.description)
+        
+        //stop the location service?
+        locationManager.stopUpdatingLocation()
+        
+        //update the database
+        sendDatatoServer("-666.666666", latitude: "66.666666", date: "2015-07-04" )
+    }
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        println("Error while updating location: " + error.localizedDescription)
+    }
+    
+    @IBAction func findMyLocation(sender:AnyObject) {
+        println("inside findMyLocation")
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+    }
+    
+    //TODO: add extra code for iOS 7 support
+    
+    // send the longitude, latitude, and date to the database server
+    func sendDatatoServer(longitude: String!, latitude: String!, date: String!) {
+        
+        println("inside sendDataToServer")
+        
+        Alamofire.request(.POST, "http://10.0.2.2/index.php", parameters:["latitude": latitude, "longitude": longitude, "date": date])
+            .responseString { (_, _, string, _) in
+                println(string)
+                
+        }
+        
+        /*
+        let request = NSMutableURLRequest(URL: NSURL(string: "http://10.0.2.2/index.php")!)
+        request.HTTPMethod = "POST"
+        
+        let postString = "longitude=" + longitude + "&latitude=" + latitude + "&date=" + date
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+            if error != nil {
+                println("error=\(error)")
+                return
+            }
+            
+            println("response = \(response)")
+            
+            let responseString = NSString(data: data, encoding: NSUTF8StringEncoding)
+            println("responseString = \(responseString)")
+        }
+        task.resume()
+*/
+    }
 
 }
 
---
+++ Mosquito/readme.txt
@@ -0,0 +1,9 @@
+Useful steps to simulate location in emulator: http://rshankar.com/get-your-current-address-in-swift/
+
+About Swift: https://developer.apple.com/library/ios/documentation/Swift/Conceptual/Swift_Programming_Language/index.html#//apple_ref/doc/uid/TP40014097-CH3-ID0
+
+Cocoapods website (includes installation guide): https://guides.cocoapods.org/
+
+Alamofire site: https://github.com/Alamofire/Alamofire
+
+iOS 8 Core Location info: http://nshipster.com/core-location-in-ios-8/
